1f5e3f24a541f6ada345a8b9cac6387be662a334
Saravanan09d <111735144+Saravanan09d@users.noreply.github.com>
2023-12-09 12:43:12 +0530

    new gitignore chnages

---
+++ UserService/UserService/Controllers/AccessController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserService.Models.DTO;
+using UserService.Services;
+
+namespace UserService.Controllers
+{
+    public class AccessController : Controller
+    {
+
+            private readonly AccessService _accessService;
+
+            public AccessController(AccessService accessService)
+            {
+                _accessService = accessService;
+            }
+
+            [HttpPost("login")]
+            public async Task<IActionResult> Login([FromBody] LoginViewModelDTO model)
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var isAuthenticated = await _accessService.AuthenticateAsync(model.Name, model.Password);
+
+                if (isAuthenticated)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    // Authentication failed
+                    return BadRequest("Invalid credentials");
+                }
+            }
+    }
+}
---
+++ UserService/UserService/Services/AccessService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using UserService.Models;
+
+namespace UserService.Services
+{
+    public class AccessService
+    {
+        private readonly UserManager<UserTableModel> _userManager;
+        private readonly SignInManager<UserRoleModel> _signInManager;
+
+        public AccessService(UserManager<UserTableModel> userManager, SignInManager<UserRoleModel> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<bool> AuthenticateAsync(string Name, string password)
+        {
+            var user = await _userManager.FindByEmailAsync(Name);
+
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(Name, password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
