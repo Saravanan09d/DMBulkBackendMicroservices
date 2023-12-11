58b94654536d74d041510d20448c43d4ba39951e
Saravanan09d <111735144+Saravanan09d@users.noreply.github.com>
2023-12-11 17:18:28 +0530

    UserService After Bug Fixing

--- UserService/Controllers/AccessController.cs
+++ UserService/Controllers/AccessController.cs
@@ -1,38 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using UserService.Models.DTO;
 using UserService.Services;
+using UserService.Model.DTO;
 
 namespace UserService.Controllers
 {
     public class AccessController : Controller
     {
+        private readonly AccessService _accessService;
 
-            private readonly AccessService _accessService;
+        public AccessController(AccessService accessService)
+        {
+            _accessService = accessService;
+        }
 
-            public AccessController(AccessService accessService)
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModelDTO model)
+        {
+            if (!ModelState.IsValid)
             {
-                _accessService = accessService;
+                return BadRequest(ModelState);
             }
-
-            [HttpPost("login")]
-            public async Task<IActionResult> Login([FromBody] LoginViewModelDTO model)
+            var userDetailsDTO = await _accessService.AuthenticateAsync(model.Email, model.Password);
+            if (userDetailsDTO != null)
             {
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
-                }
-
-                var isAuthenticated = await _accessService.AuthenticateAsync(model.Name, model.Password);
-
-                if (isAuthenticated)
-                {
-                    return Ok();
-                }
-                else
-                {
-                    // Authentication failed
-                    return BadRequest("Invalid credentials");
-                }
+                return Ok(userDetailsDTO);
             }
+            else
+            {
+                return BadRequest("Invalid credentials");
+            }
+        }
+
+        [HttpPost("createUser")]
+        public async Task<IActionResult> CreateUser([FromBody] UserTableModelDTO userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var createdUser = await _accessService.CreateUserAsync(userModel);
+            if (createdUser != null)
+            {
+                return Ok(createdUser);
+            }
+            else
+            {
+                return BadRequest("Failed to create user. Check role details.");
+            }
+        }
     }
 }
